53de5872fa8aa4893681d16b12a8223e697dd73d
mdrdani <sudokill3004@gmail.com>
2021-06-16 14:00:03 +0700

    Custom View Blade SIMASET

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,33 @@
+@include('partials.head')
+
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>{{ config('app.name', 'Laravel') }}</span></a>
+            </div>
+
+            <div class="clearfix"></div>
+
+            @include('partials.profile-quick')
+
+            <br />
+
+            @include('partials.sidebar')
+          </div>
+        </div>
+
+        @include('partials.top-nav')
+
+        @yield('body')
+
+        @include('partials.footer')
+      </div>
+    </div>
+
+    @include('partials.js')
+  </body>
+  
+</html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            SIMASET Sekolah Darma Bangsa by <a href="http://darmabangsa.sch.id">Dep. IT</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
